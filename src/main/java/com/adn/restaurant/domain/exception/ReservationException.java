1dc9f47b4f5472c53b3cd5a99abdc7e21a1b9e7e
SamirMendoza <samir.mendoza@ceiba.com.co>
2020-02-17 18:18:46 -0500

    actualizacion excepciones
    actualzacion

--- src/main/java/com/adn/restaurant/domain/exception/ReservationException.java
+++ src/main/java/com/adn/restaurant/domain/exception/ReservationException.java
@@ -1,9 +1,9 @@
 package com.adn.restaurant.domain.exception;
 
 public class ReservationException extends RuntimeException{
-	
-	private static final long serialVersionUID = 1L;
 		
+	private static final long serialVersionUID = 5434659474424687483L;
+
 		public ReservationException(String message) {
 			super(message);
 		}
