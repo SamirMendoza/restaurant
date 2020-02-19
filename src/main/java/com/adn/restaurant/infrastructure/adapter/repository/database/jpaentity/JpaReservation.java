6a7ca0cc01f8ed7178250431f22960f5d18a5fa1
SamirMendoza <samir.mendoza@ceiba.com.co>
2020-02-19 12:11:41 -0500

    eliminando duplicidad jpa

--- src/main/java/com/adn/restaurant/infrastructure/adapter/repository/database/jpaentity/JpaReservation.java
+++ src/main/java/com/adn/restaurant/infrastructure/adapter/repository/database/jpaentity/JpaReservation.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import com.adn.restaurant.domain.model.Reservation;
 
@@ -13,35 +14,16 @@ public class JpaReservation extends Reservation{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long code;
+	
+	@NotNull
 	private Long idTable;
+	
+	@NotNull
 	private double price;
+	
+	@NotNull
 	private String name;
 	
-	
-	public Long getCode() {
-		return code;
-	}
-	public void setCode(Long code) {
-		this.code = code;
-	}
-	public Long getIdTable() {
-		return idTable;
-	}
-	public void setIdTable(Long idTable) {
-		this.idTable = idTable;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public JpaReservation() {
 	}
 	
--- src/main/java/com/adn/restaurant/infrastructure/adapter/repository/database/jpaentity/JpaTable.java
+++ src/main/java/com/adn/restaurant/infrastructure/adapter/repository/database/jpaentity/JpaTable.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import com.adn.restaurant.domain.model.Table;
 
@@ -16,32 +17,11 @@ public class JpaTable extends Table{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotNull
 	private boolean availability;
 	
-	public JpaTable(Long id, boolean availability) {
-		this.id = id;
-		this.availability = availability;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public boolean isAvailability() {
-		return availability;
-	}
-
-	public void setAvailability(boolean availability) {
-		this.availability = availability;
-	}
-
 	public JpaTable() {
 	}
-	
-	
 	
 }
