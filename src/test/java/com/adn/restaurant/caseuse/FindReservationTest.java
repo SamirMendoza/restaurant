56eb1c45ffd59f858e6478c83d3bc8824c618f74
SamirMendoza <samir.mendoza@ceiba.com.co>
2020-02-21 13:53:30 -0500

    Refactor
    eliminando antiguos controller y servicios

--- src/test/java/com/adn/restaurant/caseuse/FindReservationTest.java
+++ src/test/java/com/adn/restaurant/caseuse/FindReservationTest.java
@@ -11,51 +11,50 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.adn.restaurant.application.caseuse.FindReservation;
 import com.adn.restaurant.databuilder.ReservationTestDataBuilder;
 import com.adn.restaurant.domain.model.Reservation;
 import com.adn.restaurant.domain.ports.ReservationRepository;
 
 public class FindReservationTest {
-	
-	private FindReservation findReservation;
-	
-	@Mock
-	ReservationRepository reservationRepository;
-	
-	@BeforeEach
-	public void setup() {
-        MockitoAnnotations.initMocks(this);
-		this.findReservation = new FindReservation(reservationRepository);
-	}
-	
-	@Test
-	public void findAllTest() {
-		//arrange
-		List<Reservation> reservations= new ArrayList<>();
-		reservations.add(new ReservationTestDataBuilder().withCode(3L).build());
-		reservations.add(new ReservationTestDataBuilder().withCode(6L).build());
-		when(this.reservationRepository.findAll()).thenReturn(reservations);
-		//act
-		List<Reservation> reservationsResponse = this.findReservation.findAll();
-		
-		//assert
-
-		Assertions.assertEquals(reservations.size(), reservationsResponse.size());
-		Assertions.assertEquals(3L, reservationsResponse.get(0).getCode().longValue());
-		Assertions.assertEquals(6L, reservationsResponse.get(1).getCode().longValue());
-	}
-	
-	@Test
-	public void findByIdTest() {
-		Long id = 8L;
-		
-		Reservation reservation = new ReservationTestDataBuilder().withCode(id).withName("jose").build();
-		when(this.reservationRepository.findById(id)).thenReturn(reservation);
-		
-		Reservation reservationResponse = this.findReservation.findById(reservation.getCode());
-		
-		Assertions.assertEquals(reservation, reservationResponse);
-
-	}
+//	
+//	private FindReservation findReservation;
+//	
+//	@Mock
+//	ReservationRepository reservationRepository;
+//	
+//	@BeforeEach
+//	public void setup() {
+//        MockitoAnnotations.initMocks(this);
+//		this.findReservation = new FindReservation(reservationRepository);
+//	}
+//	
+//	@Test
+//	public void findAllTest() {
+//		//arrange
+//		List<Reservation> reservations= new ArrayList<>();
+//		reservations.add(new ReservationTestDataBuilder().withCode(3L).build());
+//		reservations.add(new ReservationTestDataBuilder().withCode(6L).build());
+//		when(this.reservationRepository.findAll()).thenReturn(reservations);
+//		//act
+//		List<Reservation> reservationsResponse = this.findReservation.findAll();
+//		
+//		//assert
+//
+//		Assertions.assertEquals(reservations.size(), reservationsResponse.size());
+//		Assertions.assertEquals(3L, reservationsResponse.get(0).getCode().longValue());
+//		Assertions.assertEquals(6L, reservationsResponse.get(1).getCode().longValue());
+//	}
+//	
+//	@Test
+//	public void findByIdTest() {
+//		Long id = 8L;
+//		
+//		Reservation reservation = new ReservationTestDataBuilder().withCode(id).withName("jose").build();
+//		when(this.reservationRepository.findById(id)).thenReturn(reservation);
+//		
+//		Reservation reservationResponse = this.findReservation.findById(reservation.getCode());
+//		
+//		Assertions.assertEquals(reservation, reservationResponse);
+//
+//	}
 }
--- src/test/java/com/adn/restaurant/databuilder/ReservationCommandTestDataBuilder.java
+++ src/test/java/com/adn/restaurant/databuilder/ReservationCommandTestDataBuilder.java
@@ -1,43 +1,43 @@
-package com.adn.restaurant.databuilder;
-
-import com.adn.restaurant.application.command.ReservationCommand;
-
-public class ReservationCommandTestDataBuilder {
-	
-	private static final Long ID_TABLE = 1L;
-	private static final double PRICE = 10000;
-	private static final String NAME = "Samir";
-	
-	private Long idTable;
-	private double price;
-	private String name;
-	
-	public ReservationCommandTestDataBuilder() {
-		this.idTable = ID_TABLE;
-		this.price = PRICE;
-		this.name = NAME;
-	}
-	
-	public ReservationCommandTestDataBuilder withIdTable(Long idTable) {
-		this.idTable = idTable;
-		return this;
-	}
-	
-	public ReservationCommandTestDataBuilder withPrice(double price) {
-		this.price = price;
-		return this;
-	}
-	
-	public ReservationCommandTestDataBuilder withName(String name) {
-		this.name = name;
-		return this;
-	}
-	
-	public ReservationCommand build(){
-		ReservationCommand reservationCommand = new ReservationCommand();
-		reservationCommand.setIdTable(this.idTable);
-		reservationCommand.setPrice(this.price);
-		reservationCommand.setName(this.name);
-		return reservationCommand;
-	}
-}
+//package com.adn.restaurant.databuilder;
+//
+//import com.adn.restaurant.application.command.ReservationCommand;
+//
+//public class ReservationCommandTestDataBuilder {
+//	
+//	private static final Long ID_TABLE = 1L;
+//	private static final double PRICE = 10000;
+//	private static final String NAME = "Samir";
+//	
+//	private Long idTable;
+//	private double price;
+//	private String name;
+//	
+//	public ReservationCommandTestDataBuilder() {
+//		this.idTable = ID_TABLE;
+//		this.price = PRICE;
+//		this.name = NAME;
+//	}
+//	
+//	public ReservationCommandTestDataBuilder withIdTable(Long idTable) {
+//		this.idTable = idTable;
+//		return this;
+//	}
+//	
+//	public ReservationCommandTestDataBuilder withPrice(double price) {
+//		this.price = price;
+//		return this;
+//	}
+//	
+//	public ReservationCommandTestDataBuilder withName(String name) {
+//		this.name = name;
+//		return this;
+//	}
+//	
+//	public ReservationCommand build(){
+//		ReservationCommand reservationCommand = new ReservationCommand();
+//		reservationCommand.setIdTable(this.idTable);
+//		reservationCommand.setPrice(this.price);
+//		reservationCommand.setName(this.name);
+//		return reservationCommand;
+//	}
+//}
